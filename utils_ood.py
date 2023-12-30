5981d95cc2aab4ff45605a47de4a60d5fdd65189
fanghenshaometeor <fanghenshao@sjtu.edu.cn>
2023-12-30 10:08:28 +0800

    remove-name

--- utils_ood.py
+++ utils_ood.py
@@ -19,21 +19,21 @@ def make_id_ood(args):
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
         ])
-        args.in_datadir = '~/fangkun/data/CIFAR10'
+        args.in_datadir = '~/data/CIFAR10'
         in_set = tv.datasets.CIFAR10(args.in_datadir, train=False, transform=test_transform, download=True)
         in_set_train = tv.datasets.CIFAR10(args.in_datadir, train=True, transform=test_transform, download=True)
 
         if args.out_data == 'SVHN':
-            args.out_datadir = '~/fangkun/data/ood_data/svhn/'
+            args.out_datadir = '~/data/ood_data/svhn/'
             out_set = tv.datasets.SVHN(args.out_datadir, split='test', transform=test_transform, download=False)
         elif args.out_data in ['LSUN','iSUN','places365','LSUN_FIX','ImageNet_FIX','ImageNet_resize']:
-            args.out_datadir = '~/fangkun/data/ood_data/{}'.format(args.out_data)
+            args.out_datadir = '~/data/ood_data/{}'.format(args.out_data)
             out_set = tv.datasets.ImageFolder(args.out_datadir, transform=test_transform)
         elif args.out_data == 'Texture':
-            args.out_datadir = '~/fangkun/data/ood_data/dtd/images'
+            args.out_datadir = '~/data/ood_data/dtd/images'
             out_set = tv.datasets.ImageFolder(args.out_datadir, transform=test_transform)
         elif args.out_data == 'CIFAR100':
-            args.out_datadir = '~/fangkun/data/CIFAR100'
+            args.out_datadir = '~/data/CIFAR100'
             out_set = tv.datasets.CIFAR100(args.out_datadir, train=False, download=True, transform=test_transform)
 
 
@@ -41,9 +41,9 @@ def make_id_ood(args):
         args.in_datadir = '~/imagenet/ILSVRC2012_img_val'
         args.in_datadir_train = '~/imagenet/ILSVRC2012_img_train'
         if args.out_data == 'iNaturalist' or args.out_data == 'SUN' or args.out_data == 'Places':
-            args.out_datadir = "~/fangkun/data/ood_data/{}".format(args.out_data)
+            args.out_datadir = "~/data/ood_data/{}".format(args.out_data)
         elif args.out_data == 'Texture':
-            args.out_datadir = '~/fangkun/data/ood_data/dtd/images'
+            args.out_datadir = '~/data/ood_data/dtd/images'
 
         test_transform = transforms.Compose([
             transforms.Resize(256),
